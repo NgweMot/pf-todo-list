e8131f1f8dcd074465414c0f24c2b57d56c43666
NgweMot <ngwemotmaung@gmail.com>
2024-05-16 13:18:18 +0630

    edit img

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import empty from "../assets/empty.svg";
 const EmptyState = () => {
   return (
-    <div className="bg-white dark:bg-cyan-600">
+    <div className="bg-white dark:bg-cyan-600 sm:col-span-2">
       <img
-        className="h-auto max-w-lg rounded-lg mx-auto"
+        className="h-auto max-w-lg rounded-lg mx-auto w-72"
         src={empty}
         alt="image description"
       />
