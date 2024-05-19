71e59cf1d69626e3f98ca71cef2cacf73390f379
NgweMot <ngwemotmaung@gmail.com>
2024-05-20 01:11:00 +0630

    cmd value

--- src/components/CreateList.jsx
+++ src/components/CreateList.jsx
@@ -26,7 +26,7 @@ const CreateList = ({ addList }) => {
     <div className="sm:col-span-2 dark:bg-cyan-600">
       <div className="flex justify-between items-center gap-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
         <input
-          // value={inputValue}
+          value={inputValue}
           onChange={handleInputChange}
           onKeyUp={handleKeyup}
           type="text"
